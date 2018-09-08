40708719ed9ccf240d22a7bddf6730e67182ac7b
ziyanfeng <ziyanfeng.c@gmail.com>
2018-09-07 23:28:45 -0700

    update map format

--- gfx/src/gen_mapdata.c
+++ gfx/src/gen_mapdata.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 
-#define MAX_X 20
-#define MAX_Y 20
-#define BUF_SIZE 5599
-
-// BUF_SIZE = MAX_X * MAX_Y * 7 * 2 - 1
+#define MAX_X 15
+#define MAX_Y 15
+#define BUF_SIZE MAX_X*MAX_Y*4+6
 
 int		main(int ac, char **av)
 {
@@ -15,24 +14,39 @@ int		main(int ac, char **av)
 	int		i = 0;
 	int		j;
 	int		r;
+	char	b[9];
+	int		v;
+	int 	total = 0;
 
-	printf("%d,%d,", x, y);
+	total += printf("%d,%d,", x, y);
 	srand(time(NULL));
-	while (i < x * y * 7)
+	while (i < x * y)
 	{
-		if ((r = rand() % 10) < 10)
+		j = 0;
+		while (j < 7)
 		{
-			printf("%d,", r);
-			i++;
+			if ((r = rand() % 10) < 10)
+			{
+				if (r > 4)
+					b[j] = 1;
+				else
+					b[j] = 0;
+			}
+			else
+				continue;
+			// printf("%d ", 127);
+			j++;
 		}
-		else
-			continue;
-	}
-	while (i < MAX_X * MAX_Y * 7 - 1)
-	{
-		printf("#,");
+		v = b[0]*pow(2, 6) + b[1]*pow(2, 5) + b[2]*pow(2, 4) + 
+			b[3]*pow(2, 3) + b[4]*pow(2, 2) + b[5]*pow(2, 1) + b[6];
+		total += printf("%d,", v);
+		// printf("%d ", v);
 		i++;
 	}
-	printf("#");
+	while (total < BUF_SIZE)
+	{
+		total+=printf("#");
+	}
+	printf("total:|%i|\n", total);
 	return (0);
 }
