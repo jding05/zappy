2d2cb4cf9ae9e673d8d40e3fb5b30f2850625da2
xlz447 <xlz447@gmail.com>
2018-09-23 20:00:44 -0700

    changed init player to normal, no extra life and resources

--- server/src/util/init_player.c
+++ server/src/util/init_player.c
@@ -22,7 +22,7 @@ static void	init_live(int fd)
 	g_players[fd].live.tv_usec = curr_time.tv_usec;
 	g_players[fd].block_time.tv_sec = curr_time.tv_sec;
 	g_players[fd].block_time.tv_usec = curr_time.tv_usec;
-	update_live(fd, 1000);
+	update_live(fd, 10);
 }
 
 void		s_init_egg_player(int fd, int team_id, int egg_id)
@@ -52,12 +52,6 @@ void		s_init_new_player(int fd)
 	g_players[fd].player_id = g_player_id++;
 	g_players[fd].request_nb = 0;
 	memset(g_players[fd].inventory, 0, sizeof(int) * 7);
-	g_players[fd].inventory[1] = 9;
-	g_players[fd].inventory[2] = 8;
-	g_players[fd].inventory[3] = 10;
-	g_players[fd].inventory[4] = 5;
-	g_players[fd].inventory[5] = 6;
-	g_players[fd].inventory[6] = 1;
 	g_players[fd].y = rand() % g_env.map_y;
 	g_players[fd].x = rand() % g_env.map_x;
 	g_players[fd].level = 1;
